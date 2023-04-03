19d62801b49233b3755ceaee8159cfcdd6a2f410
Noriel <flores1noriel@gmail.com>
2023-04-03 12:18:02 -0700

    Passed all tests on all bees using ES6

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,10 @@
 class Grub {
+  constructor () {
+    this.age = 0
+    this.color = 'pink'
+    this.food = 'jelly'
+  }
 };
+Grub.prototype.eat = function () {
+  return 'Mmmmmmmmm jelly'
+}
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
   // TODO..
+  constructor () {
+    super()
+    this.age = 10
+    this.job = 'make honey'
+    this.honeyPot = 0
+    this.makeHoney = function () {
+      this.honeyPot ++
+    }
+    this.giveHoney = function () {
+      this.honeyPot --
+    }
+  }
 };
